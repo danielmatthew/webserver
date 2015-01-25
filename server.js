8ca898537bd7d02f8960c6c598241f67997e1e9b
Daniel Matthew <hello@danmatthew.co.uk>
2015-01-25 16:55:32 +0000

    Redirection

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
+var url = 'http://google.com';
+
+var body = '<p>Redirecting to <a href="' + url + '">' + url + '</a></p>';
+
 var server = http.createServer(function(req, res) {
-  res.write('Hello world');
-  res.end();
+  // res.write('Hello world');
+  res.setHeader('Location', url);
+  res.setHeader('Content-Length', body.length);
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 302;
+  res.end(body);
 });
 
 server.listen(3000);
